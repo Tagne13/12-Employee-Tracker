8120edfb63e5095de90bb75dbb9e99489d708530
El Jefe <Tagne13@gmail.com>
2023-04-26 12:04:48 -0400

    added dependencies to server; created schema for database and started seeds file to insert into new database

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'hypnotize1',
+        databse: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
