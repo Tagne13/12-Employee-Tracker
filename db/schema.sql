e5bf3bd7a107fc0a5dbb7398490c299b0ceea1bf
El Jefe <Tagne13@gmail.com>
2023-04-27 17:13:52 -0400

    added README template; added additional code to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -25,6 +26,7 @@ CREATE TABLE employee (
     manager_id INT
     FOREIGN KEY (role_id)
     REFERENCES role(id) 
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 // Dependencies
-const express = require('express');
-const mysql = require('mysql2');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express = require('express')
+const mysql = require('mysql2')
+const PORT = process.env.PORT || 3001
+const app = express()
 
 // Express middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'hypnotize1',
-        databse: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-);
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'hypnotize1',
+    databse: 'employees_db'
+  },
+  console.log('Connected to the employees_db database.')
+)
+
+// Query database
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end()
+})
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
